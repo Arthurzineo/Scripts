723edb6b22d8f41cffc0687cd069c65729a35083
Arthur Gonçalves <78982690+Arthurzineo@users.noreply.github.com>
2023-08-17 18:02:57 -0300

    Create ScriptLogoutKioskiWindows.ps1

---
+++ KioskiModeWindowsAutoLogout/ScriptLogoutKioskiWindows.ps1
@@ -0,0 +1,32 @@
+set-ExecutionPolicy -ExecutionPolicy Unrestricted -force
+
+
+$sname = 'USER NAME HERE' ;
+
+
+$sessionInfo = query session | Out-String ;
+$loggedIn = $sessionInfo -match $sname ;
+$lines = $sessionInfo -split "`n";
+$filteredSessions = $lines | Where-Object { $_ -match $sname };
+#if your language is different see the name of the active user status and change  
+$filteredSessions = $filteredSessions -match 'ATIVO';
+$filteredSessionsEN = $filteredSessions -match 'ACTIVE';
+# for PT-BR OS or ENOS
+
+if($filteredSessions -OR $filteredSessionsEN){
+    
+   exit;
+}
+else{
+    if($loggedIn){
+    
+    $sessionID = ((quser /server:$env:COMPUTERNAME | Where-Object { $_ -match $sname }) -split ' +')[2];
+    logoff $sessionID;
+    exit;
+    }
+    else {
+	  
+  	    exit;
+    }
+}
+exit;
