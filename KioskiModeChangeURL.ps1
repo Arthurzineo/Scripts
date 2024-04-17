f082076a4902b494515ddbe87ee4d0d0cadeba75
Arthur Gonçalves <78982690+Arthurzineo@users.noreply.github.com>
2024-04-17 14:58:44 -0300

    Update KioskiModeChangeURL.ps1

--- KioskiModeChangeURL.ps1
+++ KioskiModeChangeURL.ps1
@@ -4,7 +4,7 @@ $user = "usernamehere"
 $Sid = (Get-LocalUser -Name $user).SID.Value
 $profile = Get-ItemPropertyValue "HKLM:\SOFTWARE\Microsoft\Windows\AssignedAccessConfiguration\Configs\$Sid" -Name DefaultProfileId
 $regPath = "HKLM:\SOFTWARE\Microsoft\Windows\AssignedAccessConfiguration\Profiles\$profile\AllowedApps\App0"
-Set-ItemProperty -Path $regPath -Name "Arguments" -Value "--no-first-run --kiosk $webSite --kiosk-idle-timeout-minutes=0 --edge-kiosk-type=fullscreen"
+Set-ItemProperty -Path $regPath -Name "Arguments" -Value "--no-first-run --kiosk $webSite --kiosk-idle-timeout-minutes=0 --edge-kiosk-type=fullscreen" --disable-features=Translate,msWebOOUI,msPdfOOUI
 }
 
 # For use: ChangeUrl("google.com") or another site inside "". 
