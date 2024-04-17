e4647ca1304ced72a79013157b694803a5e2409b
Arthur Gonçalves <78982690+Arthurzineo@users.noreply.github.com>
2024-04-17 16:53:43 -0300

    Create KioskiModeChangeURL2.ps1

---
+++ KioskiModeChangeURL2.ps1
@@ -0,0 +1,11 @@
+$webSite = Read-Host -Prompt "Digite o URL do site"
+
+$user = "UserName"
+
+$Sid = (Get-LocalUser -Name $user).SID.Value
+
+$profile = Get-ItemPropertyValue "HKLM:\SOFTWARE\Microsoft\Windows\AssignedAccessConfiguration\Configs\$Sid" -Name DefaultProfileId
+
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\AssignedAccessConfiguration\Profiles\$profile\AllowedApps\App0"
+
+Set-ItemProperty -Path $regPath -Name "Arguments" -Value "--no-first-run --kiosk $webSite --kiosk-idle-timeout-minutes=0 --edge-kiosk-type=fullscreen --disable-features=Translate,msWebOOUI,msPdfOOUI" 
